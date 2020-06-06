c34abcb884c4867a4f5868ddc6f032f361dadd71
kartikey tyagi <kartikey110813@gmail.com>
2020-06-07 00:40:55 +0530

    Modified files

--- SpeakGoodBye.js
+++ SpeakGoodBye.js
@@ -11,17 +11,17 @@
 // var byeSpeaker =
 
 // DO NOT attach the speakWord variable to the 'byeSpeaker' object.
-var speakWord = "Good Bye";
+
 
 // STEP 8: Rewrite the 'speak' function such that it is attached to the
 // byeSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
 (function(window){
 var byeSpeaker = {};
+var speakWord = "Good Bye";
 
-byeSpeaker.names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-byeSpeaker.speak = function() {
-  console.log(speakWord + " " + byeSpeaker.names);
+byeSpeaker.speak = function(names) {
+  console.log(speakWord + " " + names);
 }
 
 window.byeSpeaker = byeSpeaker;
--- SpeakHello.js
+++ SpeakHello.js
@@ -8,18 +8,18 @@
 // var helloSpeaker =
 
 // DO NOT attach the speakWord variable to the 'helloSpeaker' object.
-var speakWord = "Hello";
+
 
 // STEP 4: Rewrite the 'speak' function such that it is attached to the
 // helloSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
 (function(window){
 var helloSpeaker = {};
+var speakWord = "Hello";
 
 
-helloSpeaker.names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-helloSpeaker.speak = function() {
-  console.log(speakWord + " " + helloSpeaker.names);
+helloSpeaker.speak = function(names) {
+  console.log(speakWord + " " + names);
   
 }
 
